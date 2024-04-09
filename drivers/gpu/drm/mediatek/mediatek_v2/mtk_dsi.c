b7651dacf11790c902eefec87527571750c43d51
Huijuan Xie <huijuan.xie@mediatek.com>
2024-04-09 22:09:09 +0800

    [ALPS08848700] drm: dsi: fix dsi1 display abnormal after esd recovery
    [Description]
    Update slave_dsi.vm.<porch> after switching framerate.
    Avoid porch differences between dsi0/1 after esd recovery.
    
    [Test]
    DSI1 display ok after ESD recovery
    
    MTK-Commit-Id: 8498fa7bf02f2f3861d0dd7910f074aa40ef45a7
    
    Signed-off-by: Huijuan Xie <huijuan.xie@mediatek.com>
    CR-Id: ALPS08848700
    Feature: Always On Display
    Change-Id: I48c31616c70df9cc7fdff3387ebdd05dee08bc70

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_dsi.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_dsi.c
@@ -9785,6 +9785,8 @@ static void mtk_dsi_vdo_timing_change(struct mtk_dsi *dsi,
 			hfp = adjusted_mode.hsync_start -
 				adjusted_mode.hdisplay;
 		dsi->vm.hfront_porch = hfp;
+		if (dsi->slave_dsi)
+			dsi->slave_dsi->vm.hfront_porch = hfp;
 
 		if (dsi->mipi_hopping_sta) {
 			DDPINFO("%s,mipi_clk_change_sta\n", __func__);
@@ -9793,6 +9795,8 @@ static void mtk_dsi_vdo_timing_change(struct mtk_dsi *dsi,
 			hbp = adjusted_mode.htotal -
 				adjusted_mode.hsync_end;
 		dsi->vm.hback_porch = hbp;
+		if (dsi->slave_dsi)
+			dsi->slave_dsi->vm.hback_porch = hbp;
 
 		if (dsi->mipi_hopping_sta && dsi->ext) {
 			DDPINFO("%s,mipi_clk_change_sta\n", __func__);
@@ -9801,6 +9805,8 @@ static void mtk_dsi_vdo_timing_change(struct mtk_dsi *dsi,
 			vfp = adjusted_mode.vsync_start -
 				adjusted_mode.vdisplay;
 		dsi->vm.vfront_porch = vfp;
+		if (dsi->slave_dsi)
+			dsi->slave_dsi->vm.vfront_porch = vfp;
 
 		/* if change vfp, get vfp, refer to hfp modification */
 		mtk_dsi_calc_vdo_timing(dsi);
@@ -9858,6 +9864,8 @@ static void mtk_dsi_vdo_timing_change(struct mtk_dsi *dsi,
 			vfp = adjusted_mode.vsync_start -
 				adjusted_mode.vdisplay;
 		dsi->vm.vfront_porch = vfp;
+		if (dsi->slave_dsi)
+			dsi->slave_dsi->vm.vfront_porch = vfp;
 
 		/* Msync 2.0 ToDo: can we change vm.vfront_porch according msync?
 		 * mmdvfs,dramdvfs according to vm.vfront_porch?
